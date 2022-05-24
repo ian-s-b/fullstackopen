924927bedbeec916fa138e3018f01f9f3c3cf8a3
ian.sobrallimeira <ian.sobrallimeira@alten.com>
2022-05-24 13:44:15 +0200

    Exercices 1.3-1.5

---
+++ notes.js
@@ -0,0 +1,10 @@
+// new_array = some_array.concat(some_value)
+// some_array.push(some_value)
+// some_array.forEach(value => {console.log(value)}) //One way of iterating through some_array items //it doesn't return anything
+// [first_value, second_value, ...rest] = some_array // unpacking values to variables // ...rest is used to collect the rest
+// object.name or object[fieldName] 
+// some_arrow_function = (var1, var2) => {return var1 + var2} // calling this function : some_arrow_function(1,5)
+// arrow_func_with_a_single_param = p => p*p // braces are not needed in this case
+// new_array = some_array.map(value => value*2) // works similarly as forEach but map returns something
+// function product(a,b) {return a*b} // one way to devalre a function
+// const product = function(a,b) {return a*b} // another way to declare a function // we can creeate anonymous functions like this
